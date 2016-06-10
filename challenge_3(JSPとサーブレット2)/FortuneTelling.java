1c993e41ca794f7066b034a18e41e4efaf6dd262
yoshinori <yoshinoriyamashita123@gmail.com>
2016-06-10 15:20:32 +0900

    Javaプログラミング　サーブレットクラスとJSPの連携　課題

---
+++ challenge_3(JSPとサーブレット2)/FortuneTelling.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.util.Random;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Date;
+import java.util.Random;
+import javax.servlet.RequestDispatcher;
+import org.camp.servlet.ResultData;
+
+/**
+ *
+ * @author yoshi
+ */
+@WebServlet(name = "FortuneTelling", urlPatterns = {"/FortuneTelling"})
+public class FortuneTelling extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        final String result = "/WEB-INF/jsp/FortuneTellingResult.jsp";
+        
+        String luckList[] = {"超大吉", "大吉", "まあまあ", "すけべ", "大好き", "大嫌い"};
+        Random rand = new Random();
+        Integer index = rand.nextInt(luckList.length);
+        
+        // リクエストスコープへ結果を設定
+        ResultData data = new ResultData();
+        data.setD(new Date());
+        data.setLuck(luckList[index]);
+        request.setAttribute("DATA", data);
+        
+        RequestDispatcher rd = request.getRequestDispatcher(result);
+        rd.forward(request, response);
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FortuneTelling</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>今日のあなたの運勢は、、、 " + luckList[index] + "です！</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
